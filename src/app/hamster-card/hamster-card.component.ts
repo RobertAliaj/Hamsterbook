3aaac0c6b4c23288b614c6b45cc8f64c8c9e83f4
rOBI <robertaliaj97@gmail.com>
2023-06-05 02:58:15 +0200

    added header

---
+++ src/app/hamster-card/hamster-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hamster-card',
+  templateUrl: './hamster-card.component.html',
+  styleUrls: ['./hamster-card.component.scss']
+})
+export class HamsterCardComponent {
+
+}
